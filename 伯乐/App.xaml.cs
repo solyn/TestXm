0e1105197535b9bf7fe27d9a2bf5288799258f14
solyn <solyn@foxmail.com>
2020-03-23 15:19:07 +0800

    1

---
+++ 伯乐/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace 伯乐
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MyTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ 伯乐/MyTabbedPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace 伯乐
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyTabbedPage : TabbedPage
+    {
+        public MyTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
